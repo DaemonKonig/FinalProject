c4591440b6c178da78d044725f8c2a08e54b1a12
Adam Scully <adam.m.scully@outlook.com>
2017-04-06 17:16:21 -0400

    Created base databases for users, inventory, and sales. Also created a new form for inventory management.

---
+++ TroopData/CookieSheet.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[CookieSheet]
+(
+	[Name] NCHAR(20) NOT NULL PRIMARY KEY, 
+    [ThinMints] INT NULL, 
+    [Caramel deLites] INT NULL, 
+    [Peanut Butter Patties] INT NULL, 
+    [Short Breat] INT NULL
+)
---
+++ TroopData/TrooperData.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[TroopDataSet]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(20) NOT NULL, 
+    [PhoneNumber] NCHAR(15) NOT NULL
+)
